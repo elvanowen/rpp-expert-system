057c9812577c3f3b5b53779f767ea931a8d40bba
trymochamad <cintaparent@gmail.com>
2016-12-12 17:37:48 +0700

    add data

--- destinations.js
+++ destinations.js
@@ -22,6 +22,7 @@ module.exports.questions = [{
   question: "Benua manakah yang ingin Anda kunjungi ?",
   options: [
           "east asia",
+          "southeast asia",
           "north america",
           "south america",
           "east europe",
@@ -63,7 +64,7 @@ module.exports.data = [{
   purpose: 'studying',
   season: 'spring',
   budget: 'sedang',
-  continent: 'east asia'
+  continent: 'southeast asia'
 }, {
   destination: 'london',
   popularity: 'sangat populer',
@@ -72,17 +73,801 @@ module.exports.data = [{
   budget: 'tinggi',
   continent: 'west europe'
 }, {
-  destination: 'gold coast australia',
+  destination: 'gold coast',
   popularity: 'cukup populer',
   purpose: 'surfing',
   season: 'summer',
   budget: 'sedang',
-  continent: 'east asia'
+  continent: 'southeast asia'
 }, {
   destination: 'bali',
   popularity: 'cukup populer',
   purpose: 'surfing',
   season: 'summer',
   budget: 'rendah',
-  continent: 'east asia'
+  continent: 'southeast asia'
+}, {
+  destination: 'jakarta',
+  popularity: 'cukup populer',
+  purpose: 'business',
+  season: 'summer',
+  budget: 'rendah',
+  continent: 'southeast asia'
+},{
+  destination: 'siam paragon thailand',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'tinggi',
+  continent: 'southeast asia'
+}, {
+  destination: 'central market malaysia',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'southeast asia'
+}, {
+  destination: 'kuta square bali',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'rendah',
+  continent: 'southeast asia'
+}, {
+  destination: 'megamall filipina',
+  popularity: 'cukup populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'tinggi',
+  continent: 'southeast asia'
+}, {
+  destination: 'grand indonesia jakarta',
+  popularity: 'cukup populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'southeast asia'
+}, {
+  destination: 'cho hom vietnam',
+  popularity: 'tidak populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'tinggi',
+  continent: 'southeast asia'
+}, {
+  destination: 'thalat kuan din laos',
+  popularity: 'tidak populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'southeast asia'
+}, {
+  destination: 'houston',
+  popularity: 'cukup populer',
+  purpose: 'shopping',
+  season: 'spring',
+  budget: 'tinggi',
+  continent: 'north america'
+}, {
+  destination: 'honolulu',
+  popularity: 'cukup populer',
+  purpose: 'shopping',
+  season: 'spring',
+  budget: 'sedang',
+  continent: 'north america'
+}, {
+  destination: 'new york',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'tinggi',
+  continent: 'north america'
+}, {
+  destination: 'los angeles',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'north america'
+}, {
+  destination: 'chicago',
+  popularity: 'cukup populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'tinggi',
+  continent: 'north america'
+},  {
+  destination: 'san francisco',
+  popularity: 'cukup populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'north america'
+}, {
+  destination: 'boston',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'autumn',
+  budget: 'tinggi',
+  continent: 'north america'
+}, {
+  destination: 'milan',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'spring',
+  budget: 'sedang',
+  continent: 'west europe'
+}, {
+  destination: 'milan',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'west europe'
+}, {
+  destination: 'paris',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'autumn',
+  budget: 'tinggi',
+  continent: 'west europe'
+}, {
+  destination: 'milan',
+  popularity: 'cukup populer',
+  purpose: 'shopping',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'west europe'
+}, {
+  destination: 'paris',
+  popularity: 'cukup populer',
+  purpose: 'shopping',
+  season: 'autumn',
+  budget: 'tinggi',
+  continent: 'west europe'
+}, {
+  destination: 'istanbul',
+  popularity: 'tidak populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'tinggi',
+  continent: 'west europe'
+}, {
+  destination: 'vienna',
+  popularity: 'tidak populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'west europe'
+}, {
+  destination: 'berlin departmentstore',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'west europe'
+}, {
+  destination: 'moscow',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'spring',
+  budget: 'tinggi',
+  continent: 'east europe'
+}, {
+  destination: 'moscow',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'tinggi',
+  continent: 'east europe'
+}, {
+  destination: 'moscow',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'winter',
+  budget: 'tinggi',
+  continent: 'east europe'
+}, {
+  destination: 'moscow',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'autumn',
+  budget: 'tinggi',
+  continent: 'east europe'
+}, {
+  destination: 'istanbul',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'spring',
+  budget: 'sedang',
+  continent: 'east europe'
+}, {
+  destination: 'istanbul',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'east europe'
+}, {
+  destination: 'istanbul',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'winter',
+  budget: 'sedang',
+  continent: 'east europe'
+}, {
+  destination: 'istanbul',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'east europe'
+}, {
+  destination: 'athens',
+  popularity: 'cukup populer',
+  purpose: 'shopping',
+  season: 'autumn',
+  budget: 'tinggi',
+  continent: 'east europe'
+}, {
+  destination: 'athens',
+  popularity: 'cukup populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'tinggi',
+  continent: 'east europe'
+}, {
+  destination: 'athens',
+  popularity: 'cukup populer',
+  purpose: 'shopping',
+  season: 'spring',
+  budget: 'tinggi',
+  continent: 'east europe'
+}, {
+  destination: 'athens',
+  popularity: 'cukup populer',
+  purpose: 'shopping',
+  season: 'winter',
+  budget: 'tinggi',
+  continent: 'east europe'
+}, {
+  destination: 'prague',
+  popularity: 'cukup populer',
+  purpose: 'shopping',
+  season: 'winter',
+  budget: 'sedang',
+  continent: 'east europe'
+}, {
+  destination: 'prague',
+  popularity: 'cukup populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'east europe'
+}, {
+  destination: 'prague',
+  popularity: 'cukup populer',
+  purpose: 'shopping',
+  season: 'spring',
+  budget: 'sedang',
+  continent: 'east europe'
+}, {
+  destination: 'prague',
+  popularity: 'cukup populer',
+  purpose: 'shopping',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'east europe'
+}, {
+  destination: 'bucharest',
+  popularity: 'tidak populer',
+  purpose: 'shopping',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'east europe'
+}, {
+  destination: 'bucharest',
+  popularity: 'tidak populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'east europe'
+}, {
+  destination: 'bucharest',
+  popularity: 'tidak populer',
+  purpose: 'shopping',
+  season: 'spring',
+  budget: 'sedang',
+  continent: 'east europe'
+}, {
+  destination: 'bucharest',
+  popularity: 'tidak populer',
+  purpose: 'shopping',
+  season: 'winter',
+  budget: 'sedang',
+  continent: 'east europe'
+}, {
+  destination: 'ecuador',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'winter',
+  budget: 'tinggi',
+  continent: 'south america'
+}, {
+  destination: 'ecuador',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'tinggi',
+  continent: 'south america'
+}, {
+  destination: 'ecuador',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'autumnn',
+  budget: 'tinggi',
+  continent: 'south america'
+}, {
+  destination: 'ecuador',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'spring',
+  budget: 'tinggi',
+  continent: 'south america'
+}, {
+  destination: 'sao paolo',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'spring',
+  budget: 'sedang',
+  continent: 'south america'
+}, {
+  destination: 'sao paolo',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'south america'
+}, {
+  destination: 'sao paolo',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'winter',
+  budget: 'sedang',
+  continent: 'south america'
+}, {
+  destination: 'sao paolo',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'south america'
+}, {
+  destination: 'chile',
+  popularity: 'kurang populer',
+  purpose: 'shopping',
+  season: 'autumn',
+  budget: 'tinggi',
+  continent: 'south america'
+}, {
+  destination: 'chile',
+  popularity: 'kurang populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'tinggi',
+  continent: 'south america'
+}, {
+  destination: 'chile',
+  popularity: 'kurang populer',
+  purpose: 'shopping',
+  season: 'winter',
+  budget: 'tinggi',
+  continent: 'south america'
+}, {
+  destination: 'chile',
+  popularity: 'kurang populer',
+  purpose: 'shopping',
+  season: 'spring',
+  budget: 'tinggi',
+  continent: 'south america'
+}, {
+  destination: 'bolivia',
+  popularity: 'kurang populer',
+  purpose: 'shopping',
+  season: 'spring',
+  budget: 'sedang',
+  continent: 'south america'
+}, {
+  destination: 'bolivia',
+  popularity: 'kurang populer',
+  purpose: 'shopping',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'south america'
+}, {
+  destination: 'bolivia',
+  popularity: 'kurang populer',
+  purpose: 'shopping',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'south america'
+}, {
+  destination: 'bolivia',
+  popularity: 'kurang populer',
+  purpose: 'shopping',
+  season: 'winter',
+  budget: 'sedang',
+  continent: 'south america'
+}, {
+  destination: 'costarica',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'winter',
+  budget: 'tinggi',
+  continent: 'south america'
+}, {
+  destination: 'costarica',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'spring',
+  budget: 'tinggi',
+  continent: 'south america'
+}, {
+  destination: 'belize',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'winter',
+  budget: 'sedang',
+  continent: 'south america'
+}, {
+  destination: 'belize',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'spring',
+  budget: 'sedang',
+  continent: 'south america'
+}, {
+  destination: 'nicaragua',
+  popularity: 'cukup populer',
+  purpose: 'honeymoon',
+  season: 'winter',
+  budget: 'tinggi',
+  continent: 'south america'
+}, {
+  destination: 'nicaragua',
+  popularity: 'cukup populer',
+  purpose: 'honeymoon',
+  season: 'spring',
+  budget: 'tinggi',
+  continent: 'south america'
+}, {
+  destination: 'uruguay',
+  popularity: 'cukup populer',
+  purpose: 'honeymoon',
+  season: 'winter',
+  budget: 'sedang',
+  continent: 'south america'
+}, {
+  destination: 'uruguay',
+  popularity: 'cukup populer',
+  purpose: 'honeymoon',
+  season: 'spring',
+  budget: 'sedang',
+  continent: 'south america'
+}, {
+  destination: 'cavas wine argentina',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'summer',
+  budget: 'tinggi',
+  continent: 'south america'
+}, {
+  destination: 'cavas wine argentina',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'autumn',
+  budget: 'tinggi',
+  continent: 'south america'
+}, {
+  destination: 'peru',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'south america'
+}, {
+  destination: 'peru',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'south america'
+}, {
+  destination: 'hawai',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'summer',
+  budget: 'tinggi',
+  continent: 'north america'
+}, {
+  destination: 'hawai',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'autumn',
+  budget: 'tinggi',
+  continent: 'north america'
+}, {
+  destination: 'kauai',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'north america'
+}, {
+  destination: 'kauai',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'north america'
+}, {
+  destination: 'lake tahoe',
+  popularity: 'cukup populer',
+  purpose: 'honeymoon',
+  season: 'summer',
+  budget: 'tinggi',
+  continent: 'north america'
+}, {
+  destination: 'lake tahoe',
+  popularity: 'cukup populer',
+  purpose: 'honeymoon',
+  season: 'autumn',
+  budget: 'tinggi',
+  continent: 'north america'
+}, {
+  destination: 'oahu',
+  popularity: 'cukup populer',
+  purpose: 'honeymoon',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'north america'
+}, {
+  destination: 'oahu',
+  popularity: 'cukup populer',
+  purpose: 'honeymoon',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'north america'
+}, {
+  destination: 'czech republic',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'summer',
+  budget: 'tinggi',
+  continent: 'east europe'
+}, {
+  destination: 'czech republic',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'autumn',
+  budget: 'tinggi',
+  continent: 'east europe'
+}, {
+  destination: 'slovenia',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'east europe'
+}, {
+  destination: 'slovenia',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'east europe'
+}, {
+  destination: 'hungary',
+  popularity: 'cukup populer',
+  purpose: 'honeymoon',
+  season: 'summer',
+  budget: 'tinggi',
+  continent: 'east europe'
+}, {
+  destination: 'hungary',
+  popularity: 'cukup populer',
+  purpose: 'honeymoon',
+  season: 'autumn',
+  budget: 'tinggi',
+  continent: 'east europe'
+}, {
+  destination: 'croatia',
+  popularity: 'cukup populer',
+  purpose: 'honeymoon',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'east europe'
+}, {
+  destination: 'croatia',
+  popularity: 'cukup populer',
+  purpose: 'honeymoon',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'east europe'
+}, {
+  destination: 'bali',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'summer',
+  budget: 'rendah',
+  continent: 'southeast asia'
+}, {
+  destination: 'bali',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'autumn',
+  budget: 'rendah',
+  continent: 'southeast asia'
+}, {
+  destination: 'phuket',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'southeast asia'
+}, {
+  destination: 'phuket',
+  popularity: 'sangat populer',
+  purpose: 'honeymoon',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'southeast asia'
+}, {
+  destination: 'con dao',
+  popularity: 'cukup populer',
+  purpose: 'honeymoon',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'southeast asia'
+}, {
+  destination: 'con dao',
+  popularity: 'cukup populer',
+  purpose: 'honeymoon',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'southeast asia'
+}, {
+  destination: 'chiang mai',
+  popularity: 'cukup populer',
+  purpose: 'honeymoon',
+  season: 'autumn',
+  budget: 'rendah',
+  continent: 'southeast asia'
+}, {
+  destination: 'chiang mai',
+  popularity: 'cukup populer',
+  purpose: 'honeymoon',
+  season: 'summer',
+  budget: 'rendah',
+  continent: 'southeast asia'
+}, {
+  destination: 'bali',
+  popularity: 'sangat populer',
+  purpose: 'surfing',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'southeast asia'
+}, {
+  destination: 'bali',
+  popularity: 'sangat populer',
+  purpose: 'surfing',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'southeast asia'
+}, {
+  destination: 'phuket',
+  popularity: 'sangat populer',
+  purpose: 'surfing',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'southeast asia'
+}, {
+  destination: 'phuket',
+  popularity: 'sangat populer',
+  purpose: 'surfing',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'southeast asia'
+}, {
+  destination: 'nha trang',
+  popularity: 'cukup populer',
+  purpose: 'surfing',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'southeast asia'
+}, {
+  destination: 'da nang',
+  popularity: 'cukup populer',
+  purpose: 'surfing',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'southeast asia'
+}, {
+  destination: 'vung tau',
+  popularity: 'cukup populer',
+  purpose: 'surfing',
+  season: 'autumn',
+  budget: 'rendah',
+  continent: 'southeast asia'
+}, {
+  destination: 'vung tau',
+  popularity: 'cukup populer',
+  purpose: 'surfing',
+  season: 'summer',
+  budget: 'rendah',
+  continent: 'southeast asia'
+}, {
+  destination: 'montauk point',
+  popularity: 'sangat populer',
+  purpose: 'surfing',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'north america'
+}, {
+  destination: 'montauk point',
+  popularity: 'sangat populer',
+  purpose: 'surfing',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'north america'
+}, {
+  destination: 'ocean city',
+  popularity: 'sangat populer',
+  purpose: 'surfing',
+  season: 'autumn',
+  budget: 'tinggi',
+  continent: 'north america'
+}, {
+  destination: 'ocean city',
+  popularity: 'sangat populer',
+  purpose: 'surfing',
+  season: 'summer',
+  budget: 'tinggi',
+  continent: 'north america'
+}, {
+  destination: 'hampthon beach',
+  popularity: 'cukup populer',
+  purpose: 'surfing',
+  season: 'summer',
+  budget: 'sedang',
+  continent: 'north america'
+}, {
+  destination: 'hampthon beach',
+  popularity: 'cukup populer',
+  purpose: 'surfing',
+  season: 'autumn',
+  budget: 'sedang',
+  continent: 'north america'
+}, {
+  destination: 'long sands',
+  popularity: 'cukup populer',
+  purpose: 'surfing',
+  season: 'autumn',
+  budget: 'rendah',
+  continent: 'north america'
+}, {
+  destination: 'long sands',
+  popularity: 'cukup populer',
+  purpose: 'surfing',
+  season: 'summer',
+  budget: 'rendah',
+  continent: 'north america'
+},{
+  destination: 'harrods london',
+  popularity: 'sangat populer',
+  purpose: 'shopping',
+  season: 'spring',
+  budget: 'tinggi',
+  continent: 'west europe'
 }]
